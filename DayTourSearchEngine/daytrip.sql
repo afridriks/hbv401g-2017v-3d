3b9d2f2d78089ec84faac8e7eab7daab58cb7d10
Einar <einar.a.helgason@gmail.com>
2017-04-12 16:54:29 +0000

    asdf

--- DayTourSearchEngine/daytrip.sql
+++ DayTourSearchEngine/daytrip.sql
@@ -5,7 +5,6 @@ DROP TABLE if exists Booking;
 DROP TABLE if exists Trip;
 DROP TABLE if exists TourCompany;
 DROP TABLE if exists Customer;
-DROP TABLE if exists Location;
 DROP TABLE if exists Type;
 DROP TABLE if exists Area;
 
@@ -32,14 +31,6 @@ INSERT INTO "Area" VALUES("Northern region");
 INSERT INTO "Area" VALUES("Southern region");
 INSERT INTO "Area" VALUES("Highlands of Iceland");
 
-
-CREATE TABLE Location (
-  areaName varchar(50) REFERENCES Area(name),
-  name varchar(50) PRIMARY KEY
-);
-
-INSERT INTO Location VALUES("Southern region", "Selfoss");
-
 CREATE TABLE Type (
   name varchar(50) PRIMARY KEY
 );
@@ -62,18 +53,18 @@ CREATE TABLE Trip (
   accessible int,
   price int,
   typeName varchar(50) REFERENCES Type(name),
-  locationName varchar(50) REFERENCES Location(name),
+  area varchar(50) REFERENCES area(name),
   maxTravelers int,
   availablePlaces int,
   tourCompanyId int REFERENCES TourCompany(id)
 );
 
-INSERT INTO "Trip" VALUES(1,'Hestaferðin','2017-06-22','10:00','13:00','Skemmtileg hestaferð',0,0,15000,"Horse Trips","Selfoss",10,10,1);
-INSERT INTO "Trip" VALUES(2,'Bjórferðin','2017-06-22','11:00','14:00','Skemmtileg bjórferð',0,0,25000,"Beer Trips","Selfoss",12,12,2);
-INSERT INTO "Trip" VALUES(3,'Eldfjallaferðin','2017-06-22','12:00','15:00','Skemmtileg eldfjallaferð',0,0,29990,"Volcano","Selfoss",5,5,3);
-INSERT INTO "Trip" VALUES(4,'Jöklaferðin','2017-06-22','13:00','16:00','Skemmtileg jöklferð',0,0,19990,"Glaciers","Selfoss",10,10,1);
-INSERT INTO "Trip" VALUES(5,'Matarferðin','2017-06-22','14:00','17:00','Skemmtileg matarferð',0,0,10000,"Food Trips","Selfoss",15,15,2);
-INSERT INTO "Trip" VALUES(6,'Gullni Hringurinn','2017-06-22','15:00','18:00','Skemmtileg fjölskylduferð',1,0,35000,"Golden Circle","Selfoss",20,20,3);
+INSERT INTO "Trip" VALUES(1,'Hestaferðin','2017-06-22','10:00','13:00','Skemmtileg hestaferð',0,0,15000,"Horse Trips","Southern region",10,10,1);
+INSERT INTO "Trip" VALUES(2,'Bjórferðin','2017-06-22','11:00','14:00','Skemmtileg bjórferð',0,0,25000,"Beer Trips","Southern region",12,12,2);
+INSERT INTO "Trip" VALUES(3,'Eldfjallaferðin','2017-06-22','12:00','15:00','Skemmtileg eldfjallaferð',0,0,29990,"Volcano","Southern region",5,5,3);
+INSERT INTO "Trip" VALUES(4,'Jöklaferðin','2017-06-22','13:00','16:00','Skemmtileg jöklferð',0,0,19990,"Glaciers","Southern region",10,10,1);
+INSERT INTO "Trip" VALUES(5,'Matarferðin','2017-06-22','14:00','17:00','Skemmtileg matarferð',0,0,10000,"Food Trips","Southern region",15,15,2);
+INSERT INTO "Trip" VALUES(6,'Gullni Hringurinn','2017-06-22','15:00','18:00','Skemmtileg fjölskylduferð',1,0,35000,"Golden Circle","Southern region",20,20,3);
 
 CREATE TABLE Customer (
   id INTEGER PRIMARY KEY,
